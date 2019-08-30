5e71c838c2396a72d735b3ac43951e7254dd1727
jotaeta <jotaetaa@gmail.com>
2019-08-29 18:42:29 -0600

    comprimir archivo

--- ArbolHuffmanZLV/Helpers/InstaDatos.cs
+++ ArbolHuffmanZLV/Helpers/InstaDatos.cs
@@ -7,5 +7,17 @@ namespace ArbolHuffmanZLV.Helpers
 {
     public class InstaDatos
     {
+        private static InstaDatos _instance = null;
+        public static InstaDatos Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new InstaDatos();
+                return _instance;
+            }
+        }
+        //public Stack <mCompresiones> = new Stack<>();
+        public LogicArchivos lArchivo= new LogicArchivos;
+
     }
 }
--- ArbolHuffmanZLV/Models/LogicArchivos.cs
+++ ArbolHuffmanZLV/Models/LogicArchivos.cs
@@ -7,5 +7,38 @@ namespace ArbolHuffmanZLV.Models
 {
     public class LogicArchivos
     {
+        public void Comprimir(string Lector)
+        {
+            List<frecuencia> lfrecuencia = new List<frecuencia>();
+            using (var lector = new StreamReader(Lector))
+            {
+                int contador = 0;
+                while (!lector.EndOfStream)
+                {
+                    contador++;
+                    String linea = Convert.ToString(lector.ReadLine());
+                    for (int i = 0; i < linea.Length; i++)
+                    {
+                        ContarLetras(linea.Substring(i, 1), ref frecuencia);
+                    }
+                }
+                if (contador > 1)
+                {
+                    frecuencia.Add(new frecuencia { letra = "/n", frecuencia = contador - 1 });
+                }
+                frecuencia.Sort();
+            }
+        }
+
+        public void ContarLetras(string letra, ref List<frecuencia> fre)
+        {
+            bool BuscarLetra = true, ExisteLetra = true;
+            int cont = 0;
+            while (BuscarLetra && ExisteLetra)
+            {
+
+            }
+        }
+
     }
 }
