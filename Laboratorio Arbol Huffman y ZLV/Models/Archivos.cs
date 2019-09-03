0131b310f69fb1f3d268b5a9acfe64398b8dd1e9
jotaeta <jotaetaa@gmail.com>
2019-09-03 17:33:11 -0600

    Diccionario

--- Laboratorio Arbol Huffman y ZLV/Models/Archivos.cs
+++ Laboratorio Arbol Huffman y ZLV/Models/Archivos.cs
@@ -10,22 +10,35 @@ namespace Laboratorio_Arbol_Huffman_y_ZLV.Models
     {
         public void Comprimir(string sPath)
         {
-            Dictionary<char, double> TablaLetras = new Dictionary<char, double>();
-
-            //using (var LeerArchivo = new FileStream(sPath, FileMode.OpenOrCreate))
-            //{
-
-            //}
+            Dictionary<byte, double> TablaLetras = new Dictionary<byte, double>();
 
             using (var stream = new FileStream(sPath, FileMode.Open))
             {
                 using (var reader = new BinaryReader(stream))
                 {
-                    var byteBuffer = new byte[10000];
+                    //Cantidad de letras en buffer
+                    const int bufferLength = 100;
+
+                    var byteBuffer = new byte[bufferLength];
                     while (reader.BaseStream.Position != reader.BaseStream.Length)
                     {
-                        byteBuffer = reader.ReadBytes(10000);
-                        //writer.Write(byteBuffer);
+                        byteBuffer = reader.ReadBytes(bufferLength);
+
+                        foreach (var letra in byteBuffer)
+                        {
+                            if (TablaLetras.ContainsKey(letra))
+                            {
+                                TablaLetras[letra]++;
+                            }
+                            else
+                            {
+                                TablaLetras.Add(letra, 1);
+                            }
+                        }
+                    }
+                    foreach (var letra in TablaLetras)
+                    {
+                        TablaLetras[letra.Key] = letra.Value / Convert.ToDouble(TablaLetras.Count);
                     }
                 }
             }
